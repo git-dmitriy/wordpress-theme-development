29e198b0661d5474e806af7d2125c1e6c7db3758
Dmitriy <shalberkind@mail.ru>
2020-08-30 22:04:28 +0300

    Добавлен многомерный массив с информацией об образовании

--- career_profile/data.php
+++ career_profile/data.php
@@ -7,5 +7,21 @@ return [
     'email' => 'box@mail.ru',
     'phone' => '+7 (987) 654-32-10',
     'site' => 'my-cite.net'
+  ],
+  'education' => [
+    [
+      'faculty' => 'Электроника и микроэлектроника',
+      'university' => 'СКГМИ',
+      'yearStart' => 2013,
+      'yearEnd' => 2017
+    ],
+    [
+      'faculty' => 'ТО Компьтеров и сетей',
+      'university' => 'ВКЭ',
+      'yearStart' => 2009,
+      'yearEnd' => 2013
+    ]
+
+
   ]
 ];
