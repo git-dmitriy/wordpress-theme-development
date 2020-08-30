4964d6c60f31a772254b5c15f1b781cb3b1060ba
Dmitriy <shalberkind@mail.ru>
2020-08-30 20:50:14 +0300

    Создание файла с данными

---
+++ career_profile/data.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'about' => [
+    'name' => 'Dmitriy Shalberkin',
+    'post' => 'WebDeveloper',
+    'email' => 'box@mail.ru',
+    'phone' => '+7 (987) 654-32-10',
+    'site' => 'my-cite.net'
+  ]
+];
