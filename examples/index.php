c2528884ae648c7d7230182903b9d065b194f659
Dmitriy <shalberkind@mail.ru>
2020-09-04 16:47:46 +0300

    Синтаксис наследования класса

--- examples/index.php
+++ examples/index.php
@@ -20,13 +20,33 @@ class Article
 
   public function publishArticle()
   {
-    echo "<div style='border: {$this->border}; background: {$this->bg}; font-size: {this->articleBodyFontSize}px;'>
-            <h2>{$this->title};</h2>
-            <p>{$this->articleBody};</p>
+    echo "<div style='border: {$this->border}; background: {$this->bg}; font-size: {$this->articleBodyFontSize}px;'>
+            <h2>{$this->title}</h2>
+            <p>{$this->articleBody}</p>
                       </div>";
   }
 }
 
-$newsItem = new Article('Спортивная новость', 20, 'lorem ipsum dollar', 14, '1px solid #2e2e2e', 'grey');
+class SportArticle extends Article
+{
+  public $image;
+}
 
-$newsItem->publishArticle();
+class WeatherArticle extends Article
+{
+  public $temperature;
+}
+
+class PoliticsArticle extends Article
+{
+  public $link;
+}
+
+$sportNews = new SportArticle('Sport news', 20, 'Sport is the foundation of health', 16, '2px dashed red', 'yellow');
+$sportNews->publishArticle();
+
+$weatherNews = new WeatherArticle('Weather news', 20, 'Weather is the foundation of leisure', 16, '2px dashed red', 'green');
+$weatherNews->publishArticle();
+
+$politicsNews = new PoliticsArticle('Politics news', 20, 'Politics is the foundation of dispute', 16, '2px dashed red', 'pink');
+$politicsNews->publishArticle();
