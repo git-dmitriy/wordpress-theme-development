7e251dc925fa7be59923665bc3b0967ab4c2a5c6
Dmitriy <shalberkind@mail.ru>
2020-09-04 12:44:25 +0300

    Знакомство с синтаксисом классов в php

---
+++ examples/index.php
@@ -0,0 +1,32 @@
+<?
+class Article
+{
+  public $title;
+  public $titleFontSize;
+  public $articleBody;
+  public $articleBodyFontSize;
+  public $border;
+  public $bg;
+
+  public function __construct($title, $titleFontSize, $articleBody, $articleBodyFontSize, $border, $bg)
+  {
+    $this->title = $title;
+    $this->titleFontSize = $titleFontSize;
+    $this->articleBody = $articleBody;
+    $this->articleBodyFontSize = $articleBodyFontSize;
+    $this->border = $border;
+    $this->bg = $bg;
+  }
+
+  public function publishArticle()
+  {
+    echo "<div style='border: {$this->border}; background: {$this->bg}; font-size: {this->articleBodyFontSize}px;'>
+            <h2>{$this->title};</h2>
+            <p>{$this->articleBody};</p>
+                      </div>";
+  }
+}
+
+$newsItem = new Article('Спортивная новость', 20, 'lorem ipsum dollar', 14, '1px solid #2e2e2e', 'grey');
+
+$newsItem->publishArticle();
