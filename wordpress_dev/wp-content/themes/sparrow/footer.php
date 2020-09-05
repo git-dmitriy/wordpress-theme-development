3e9aefed4dafd8061ae428a80c4e3a00b6e53357
Dmitriy <shalberkind@mail.ru>
2020-09-05 15:24:05 +0300

    Использование хуков для подключения скриптов

--- wordpress_dev/wp-content/themes/sparrow/footer.php
+++ wordpress_dev/wp-content/themes/sparrow/footer.php
@@ -36,18 +36,9 @@
   </div>
 
 </footer> <!-- Footer End-->
+<?php wp_footer(); ?>
+<!-- Java Script ================================================== -->
 
-<!-- Java Script
-   ================================================== -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script>
-  window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')
-</script>
-<script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
-
-<script src="js/jquery.flexslider.js"></script>
-<script src="js/doubletaptogo.js"></script>
-<script src="js/init.js"></script>
 
 </body>
 
--- wordpress_dev/wp-content/themes/sparrow/functions.php
+++ wordpress_dev/wp-content/themes/sparrow/functions.php
@@ -1,5 +1,6 @@
 <?php
 add_action('wp_enqueue_scripts', 'theme_styles');
+add_action('wp_enqueue_scripts', 'theme_scripts');
 
 function theme_styles()
 {
@@ -8,4 +9,18 @@ function theme_styles()
   wp_enqueue_style('layout', get_template_directory_uri() . '/assets/css/layout.css');
   wp_enqueue_style('fonts', get_template_directory_uri() . '/assets/css/fonts.css');
   wp_enqueue_style('media-queries', get_template_directory_uri() . '/assets/css/media-queries.css');
+}
+
+
+function theme_scripts()
+{
+  wp_deregister_script('jquery');
+  wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('flexslider', get_template_directory_uri() . '/assets/js/jquery.flexslider.js', ['jquery'], null, true);
+
+  wp_enqueue_script('doubletaptogo', get_template_directory_uri() . '/assets/js/doubletaptogo.js', ['jquery'], null, true);
+  wp_enqueue_script('init', get_template_directory_uri() . '/assets/js/init.js', ['jquery'], null, true);
+
+  wp_enqueue_script('modernizr', get_template_directory_uri() . '/assets/js/modernizr.js', null, null, false);
 }
