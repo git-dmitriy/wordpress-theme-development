24107c473471e72dc1ab6f59f9dce5b18282774e
Dmitriy <shalberkind@mail.ru>
2020-09-06 23:20:30 +0300

    Вывод меню подвала при помощи функции wp_nav_menu

--- wordpress_dev/wp-content/themes/sparrow/footer.php
+++ wordpress_dev/wp-content/themes/sparrow/footer.php
@@ -6,14 +6,17 @@
 
     <div class="twelve columns">
 
-      <ul class="footer-nav">
-        <li><a href="#">Home.</a></li>
-        <li><a href="#">Blog.</a></li>
-        <li><a href="#">Portfolio.</a></li>
-        <li><a href="#">About.</a></li>
-        <li><a href="#">Contact.</a></li>
-        <li><a href="#">Features.</a></li>
-      </ul>
+      <?php
+
+      wp_nav_menu([
+        'theme_location'  => 'footer',
+        'container'       => 'false',
+        'menu_class'      => 'footer-nav',
+      ]);
+      ?>
+
+
+
 
       <ul class="footer-social">
         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
