b5b27a666aa02d3302cacaf3a293c4c0c8f57fa3
Dmitriy <shalberkind@mail.ru>
2020-09-07 00:04:14 +0300

    Регистрация правого меню

---
+++ wordpress_dev/wp-content/themes/sparrow/sidebar.php
@@ -0,0 +1,58 @@
+<aside id="sidebar">
+
+  <div class="widget widget_search">
+    <h5>Search</h5>
+    <form action="#">
+
+      <input class="text-search" type="text" onfocus="if (this.value == 'Search here...') { this.value = ''; }" onblur="if(this.value == '') { this.value = 'Search here...'; }" value="Search here...">
+      <input type="submit" class="submit-search" value="">
+
+    </form>
+  </div>
+
+  <div class="widget widget_text">
+    <h5 class="widget-title">Text Widget</h5>
+    <div class="textwidget">Proin gravida nibh vel velit auctor aliquet.
+      Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum,
+      nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus
+      a sit amet mauris. Morbi accumsan ipsum velit. </div>
+  </div>
+
+  <div class="widget widget_categories">
+    <h5 class="widget-title">Categories</h5>
+    <ul class="link-list cf">
+      <li><a href="#">Designs</a></li>
+      <li><a href="#">Internet</a></li>
+      <li><a href="#">Typography</a></li>
+      <li><a href="#">Photography</a></li>
+      <li><a href="#">Web Development</a></li>
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">Other Stuff</a></li>
+    </ul>
+  </div>
+
+  <div class="widget widget_tag_cloud">
+    <h5 class="widget-title">Tags</h5>
+    <div class="tagcloud cf">
+      <a href="#">drupal</a>
+      <a href="#">joomla</a>
+      <a href="#">ghost</a>
+      <a href="#">wordpress</a>
+    </div>
+  </div>
+
+  <div class="widget widget_photostream">
+    <h5>Photostream</h5>
+    <ul class="photostream cf">
+      <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+      <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+      <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+      <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+      <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+      <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+      <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+      <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+    </ul>
+  </div>
+
+</aside>
