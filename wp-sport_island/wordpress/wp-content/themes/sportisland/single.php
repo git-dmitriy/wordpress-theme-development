5bf7bc377a7a916249e59cd1e2bb15c7984029dd
Dmitriy <shalberkind@mail.ru>
2020-10-13 00:26:05 +0300

    Разметка для страницы "Публикация"

---
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/single.php
@@ -0,0 +1,82 @@
+<?php
+get_header();
+?>
+
+<main class="main-content">
+  <div class="wrapper">
+    <ul class="breadcrumbs">
+      <li class="breadcrumbs__item breadcrumbs__item_home">
+        <a href="index.html" class="breadcrumbs__link">Главная</a>
+      </li>
+      <li class="breadcrumbs__item">
+        <a href="blog.html" class="breadcrumbs__link">Блог</a>
+      </li>
+      <li class="breadcrumbs__item">
+        <a href="category.html" class="breadcrumbs__link">Кардио</a>
+      </li>
+      <li class="breadcrumbs__item">
+        <a href="single.html" class="breadcrumbs__link">Рельефный пресс</a>
+      </li>
+    </ul>
+  </div>
+  <article class="main-article wrapper">
+    <header class="main-article__header">
+      <img src="img/blog__article_full.jpg" alt="" class="main-article__thumb">
+      <h1 class="main-article__h">Рельефный пресс</h1>
+    </header>
+    <p> Таким образом новая модель организационной деятельности позволяет оценить значение системы обучения кадров, соответствует насущным потребностям. Идейные соображения высшего порядка, а также постоянный количественный рост и сфера нашей активности обеспечивает широкому кругу (специалистов) участие в формировании модели развития. </p>
+    <p> Значимость этих проблем настолько очевидна, что новая модель организационной деятельности в значительной степени обуславливает создание модели развития. Разнообразный и богатый опыт постоянный количественный рост и сфера нашей активности способствует подготовки и реализации систем массового участия. Идейные соображения высшего порядка, а также дальнейшее развитие различных форм деятельности в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям. </p>
+    <footer class="main-article__footer">
+      <time datetime="01-01-2020">1 января</time>
+      <a href="#" class="main-article__like like">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 51.997 51.997" style="enable-background:new 0 0 51.997 51.997;" xml:space="preserve">
+          <style>
+            path {
+              fill: #666;
+            }
+          </style>
+          <path d="M51.911,16.242C51.152,7.888,45.239,1.827,37.839,1.827c-4.93,0-9.444,2.653-11.984,6.905
+	c-2.517-4.307-6.846-6.906-11.697-6.906c-7.399,0-13.313,6.061-14.071,14.415c-0.06,0.369-0.306,2.311,0.442,5.478
+	c1.078,4.568,3.568,8.723,7.199,12.013l18.115,16.439l18.426-16.438c3.631-3.291,6.121-7.445,7.199-12.014
+	C52.216,18.553,51.97,16.611,51.911,16.242z" />
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+          <g>
+          </g>
+        </svg>
+        <span class="like__text">Нравится </span>
+        <span class="like__count">46</span>
+      </a>
+    </footer>
+  </article>
+</main>
+
+<?php
+get_footer();
+?>
