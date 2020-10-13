ba78baa3bf6973d60cd9d2a943ff85208e94f5a5
Dmitriy <shalberkind@mail.ru>
2020-10-13 15:25:06 +0300

    Подключение стилей

--- wp-sport_island/wordpress/wp-content/themes/sportisland/functions.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/functions.php
@@ -1,6 +1,8 @@
 <?php
 
 add_action('after_setup_theme', 'si_setup');
+// // add_filter('show_admin_bar', '__return_false');
+add_action('wp_enqueue_scripts', 'si_scripts');
 
 function si_setup()
 {
@@ -9,7 +11,13 @@ function si_setup()
   add_theme_support('post-thumbnails');
 }
 
+function si_scripts()
+{
+  // wp_enqueue_scripts('js', get_template_directory_uri() . '/assets/js/js.js', [], '1.0', true);
+  wp_enqueue_style('si-styles', get_template_directory_uri() . '/assets/css/styles.css', [], '1.0', 'all');
+}
+
 function _si_assets_path($path)
 {
-  return get_template_directory_uri() . 'assets/' . $path;
+  echo get_template_directory_uri() . '/assets/' . $path;
 }
