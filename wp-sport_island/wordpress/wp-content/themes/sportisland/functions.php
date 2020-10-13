79184e19bb085bf7fdecaac12ea96dd496ff6195
Dmitriy <shalberkind@mail.ru>
2020-10-13 11:52:29 +0300

    Добавлена функция кастомных title и миниатюр для записей

--- wp-sport_island/wordpress/wp-content/themes/sportisland/functions.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/functions.php
@@ -5,6 +5,8 @@ add_action('after_setup_theme', 'si_setup');
 function si_setup()
 {
   add_theme_support('custom-logo');
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
 }
 
 function _si_assets_path($path)
