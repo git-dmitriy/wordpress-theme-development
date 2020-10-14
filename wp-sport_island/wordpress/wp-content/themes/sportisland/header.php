26ae76eb00ebce5c257db5785097afcfa930ab05
Dmitriy <shalberkind@mail.ru>
2020-10-14 21:54:50 +0300

    Подключение области для сайдбара в шапке

--- wp-sport_island/wordpress/wp-content/themes/sportisland/header.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/header.php
@@ -25,10 +25,16 @@
         'items_wrap' => '<ul class="%2$s">%3$s</ul>',
       ]) ?>
 
-      <address class="main-header__widget widget-contacts">
+      <!-- <address class="main-header__widget widget-contacts">
         <a href="tel:88007003030" class="widget-contacts__phone"> 8 800 700 30 30 </a>
         <p class="widget-contacts__address"> ул. Приречная 11 </p>
-      </address>
+      </address> -->
+
+      <?php
+      if (is_active_sidebar('si-header')) {
+        dynamic_sidebar('si-header');
+      }
+      ?>
       <button class="main-header__mobile">
         <span class="sr-only">Открыть мобильное меню</span>
       </button>
