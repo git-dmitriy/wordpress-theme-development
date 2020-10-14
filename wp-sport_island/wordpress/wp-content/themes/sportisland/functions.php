a3582a6325826fc57b328ee7a307bed52d717bb2
Dmitriy <shalberkind@mail.ru>
2020-10-14 21:51:38 +0300

    Регистрация сайдбара в шапке сайта

--- wp-sport_island/wordpress/wp-content/themes/sportisland/functions.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/functions.php
@@ -3,6 +3,7 @@
 add_action('after_setup_theme', 'si_setup');
 // // add_filter('show_admin_bar', '__return_false');
 add_action('wp_enqueue_scripts', 'si_scripts');
+add_action('widgets_init', 'si_register');
 
 function si_setup()
 {
@@ -18,6 +19,16 @@ function si_scripts()
 {
   wp_enqueue_script('js', _si_assets_path('js/js.js'), [], '1.0', true);
   wp_enqueue_style('si-styles', _si_assets_path('css/styles.css'), [], '1.0', 'all');
+}
+
+function si_register()
+{
+  register_sidebar([
+    'name' => 'Сайдбар в шапке сайта',
+    'id' => 'si-header',
+    'before_widget' => null,
+    'after_widget' => null
+  ]);
 }
 
 function _si_assets_path($path)
