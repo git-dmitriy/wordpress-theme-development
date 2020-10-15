5dfde3c34113163a16f8bcb642c108862252879a
Dmitriy <shalberkind@mail.ru>
2020-10-15 13:09:17 +0300

    Подключен виджет конактов в шапку и подвал

--- wp-sport_island/wordpress/wp-content/themes/sportisland/footer.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/footer.php
@@ -41,12 +41,6 @@
         'items_wrap' => '<ul class="%2$s">%3$s</ul>'
       ])
       ?>
-
-
-      <!-- <address class="main-header__widget widget-contacts">
-        <a href="tel:88007003030" class="widget-contacts__phone"> 8 800 700 30 30 </a>
-        <p class="widget-contacts__address"> ул. Приречная 11 </p>
-      </address> -->
       <?php
       if (is_active_sidebar('si-footer-contacts')) {
         dynamic_sidebar('si-footer-contacts');
