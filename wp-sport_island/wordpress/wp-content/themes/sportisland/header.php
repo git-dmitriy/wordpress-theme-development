c7ef1b1ea200162fb211ab80d36c7dbaee174b66
Dmitriy <shalberkind@mail.ru>
2020-10-20 14:04:56 +0300

    Удален тег title нарушающий работу функции title-tag

--- wp-sport_island/wordpress/wp-content/themes/sportisland/header.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/header.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title> Главная </title>
+  
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,800,900&display=swap&subset=cyrillic" rel="preload stylesheet">
   <?php wp_head(); ?>
   <!-- <link rel="stylesheet" href="./css/styles.css"> -->
