b671e03971eaad2a75f0e8defb0490d9ecf6fa21
Dmitriy <shalberkind@mail.ru>
2020-10-20 16:45:10 +0300

    Добавлен класс тега body
    Для всех страниц кроме главной

--- wp-sport_island/wordpress/wp-content/themes/sportisland/header.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/header.php
@@ -7,30 +7,38 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-  
+
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,800,900&display=swap&subset=cyrillic" rel="preload stylesheet">
   <?php wp_head(); ?>
-  <!-- <link rel="stylesheet" href="./css/styles.css"> -->
 </head>
+<?php
+if (is_front_page()) :
+?>
 
-<body>
-  <header class="main-header">
-    <div class="wrapper main-header__wrap">
-      <?php the_custom_logo(); ?>
-      <?php wp_nav_menu([
-        'theme_location' => 'menu-header',
-        'container' => 'nav',
-        'container_class' => 'main-navigation',
-        'menu_class' => 'main-navigation__list',
-        'items_wrap' => '<ul class="%2$s">%3$s</ul>',
-      ]) ?>
-      <?php
-      if (is_active_sidebar('si-header-contacts')) {
-        dynamic_sidebar('si-header-contacts');
-      }
-      ?>
-      <button class="main-header__mobile">
-        <span class="sr-only">Открыть мобильное меню</span>
-      </button>
-    </div>
-  </header>
+  <body>
+  <?php
+else :
+  ?>
+
+    <body class="inner">
+    <?php endif; ?>
+    <header class="main-header">
+      <div class="wrapper main-header__wrap">
+        <?php the_custom_logo(); ?>
+        <?php wp_nav_menu([
+          'theme_location' => 'menu-header',
+          'container' => 'nav',
+          'container_class' => 'main-navigation',
+          'menu_class' => 'main-navigation__list',
+          'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+        ]) ?>
+        <?php
+        if (is_active_sidebar('si-header-contacts')) {
+          dynamic_sidebar('si-header-contacts');
+        }
+        ?>
+        <button class="main-header__mobile">
+          <span class="sr-only">Открыть мобильное меню</span>
+        </button>
+      </div>
+    </header>
