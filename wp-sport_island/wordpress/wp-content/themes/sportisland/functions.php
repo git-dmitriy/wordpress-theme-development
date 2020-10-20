b1e3ac1555a0daefc7cf565671b776ba8243889e
Dmitriy <shalberkind@mail.ru>
2020-10-20 10:54:39 +0300

    Добавлен шорткод
    Добавляет ссылку с указанием типа и протокола

--- wp-sport_island/wordpress/wp-content/themes/sportisland/functions.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/functions.php
@@ -18,6 +18,9 @@ add_action('after_setup_theme', 'si_setup');
 // // add_filter('show_admin_bar', '__return_false');
 add_action('wp_enqueue_scripts', 'si_scripts');
 add_action('widgets_init', 'si_register');
+add_shortcode('si-paste-link', 'si_paste_link');
+
+add_filter('si_widget_text', 'do_shortcode');
 
 function si_setup()
 {
@@ -33,6 +36,37 @@ function si_scripts()
 {
   wp_enqueue_script('js', _si_assets_path('js/js.js'), [], '1.0', true);
   wp_enqueue_style('si-styles', _si_assets_path('css/styles.css'), [], '1.0', 'all');
+}
+
+function si_paste_link($attr)
+{
+  $params = shortcode_atts([
+    'link' => '',
+    'text' => '',
+    'type' => 'link',
+  ], $attr);
+  $params['text'] = $params['text'] ? $params['text'] : $params['link'];
+  if ($params['link']) {
+    $protocol = '';
+    switch ($params['type']) {
+      case 'email':
+        $protocol = 'mailto:';
+        break;
+      case 'phone':
+        $protocol = 'tel:';
+        $params['link'] = preg_replace('/[^+0-9]/', '', $params['link']);
+        break;
+      default:
+        $protocol = '';
+        break;
+    }
+    $link = $protocol . $params['link'];
+    $text = $params['text'];
+
+    return "<a href=\"${link}\">${text}</a>";
+  } else {
+    return '';
+  }
 }
 
 function si_register()
