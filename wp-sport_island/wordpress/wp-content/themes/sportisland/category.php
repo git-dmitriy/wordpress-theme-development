173791929de1277d3c89ac573555bc39325921a5
Dmitriy <shalberkind@mail.ru>
2020-10-20 21:42:21 +0300

    Наполнение страницы категории

--- wp-sport_island/wordpress/wp-content/themes/sportisland/category.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/category.php
@@ -1,85 +1,50 @@
 <?php
 get_header();
+$title = single_cat_title('', false);
 ?>
 
 <main class="main-content">
-  <h1 class="sr-only"> Страница категории Кардио в блоге сайта спортклуба SportIsland </h1>
+  <h1 class="sr-only"> Страница категории <?php echo $title; ?> в блоге сайта спортклуба SportIsland </h1>
   <div class="wrapper">
-    <ul class="breadcrumbs">
-      <li class="breadcrumbs__item breadcrumbs__item_home">
-        <a href="index.html" class="breadcrumbs__link">Главная</a>
-      </li>
-      <li class="breadcrumbs__item">
-        <a href="blog.html" class="breadcrumbs__link">Блог</a>
-      </li>
-      <li class="breadcrumbs__item">
-        <a href="category.html" class="breadcrumbs__link">Кардио</a>
-      </li>
-    </ul>
+    <?php get_template_part('tmp/breadcrumbs'); ?>
   </div>
-  <section class="category-posts">
-    <div class="wrapper">
-      <h2 class="main-heading category-posts__h"> кардио </h2>
-      <ul class="posts-list">
-        <li class="last-post">
-          <a href="single.html" class="last-post__link" aria-label="Читать текст статьи: Растяжка от болей в мышцах">
-            <figure class="last-post__thumb">
-              <img src="./img/blog__article_thmb1.jpg" alt="" class="last-post__img">
-            </figure>
-            <div class="last-post__wrap">
-              <h3 class="last-post__h"> Растяжка от болей в мышцах </h3>
-              <p class="last-post__text"> Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. </p>
-              <span class="last-post__more link-more">Подробнее</span>
-            </div>
-          </a>
-        </li>
-        <li class="last-post">
-          <a href="single.html" class="last-post__link" aria-label="Читать текст статьи: Бег помогает похудеть">
-            <figure class="last-post__thumb">
-              <img src="./img/blog__article_thmb2.jpg" alt="" class="last-post__img">
-            </figure>
-            <div class="last-post__wrap">
-              <h3 class="last-post__h"> Бег помогает похудеть </h3>
-              <p class="last-post__text"> Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. </p>
-              <span class="last-post__more link-more">Подробнее</span>
-            </div>
-          </a>
-        </li>
-        <li class="last-post">
-          <a href="single.html" class="last-post__link" aria-label="Читать текст статьи: Рельефный пресс">
-            <figure class="last-post__thumb">
-              <img src="./img/blog__article_thmb3.jpg" alt="" class="last-post__img">
-            </figure>
-            <div class="last-post__wrap">
-              <h3 class="last-post__h"> Рельефный пресс </h3>
-              <p class="last-post__text"> Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. </p>
-              <span class="last-post__more link-more">Подробнее</span>
-            </div>
-          </a>
-        </li>
-      </ul>
-      <nav class="pagination">
-        <h3 class="sr-only">Навигация</h3>
-        <ul class="pagination__links">
-          <li class="pagination__block pagination__block_current">
-            <a href="#" class="pagination__link"> 1 </a>
-          </li>
-          <li class="pagination__block">
-            <a href="#" class="pagination__link"> 2 </a>
-          </li>
-          <li class="pagination__block">
-            <a href="#" class="pagination__link"> 3 </a>
-          </li>
-          <li class="pagination__block">
-            <a href="#" class="pagination__link"> 4 </a>
-          </li>
-          <li class="pagination__block">
-            <a href="#" class="pagination__link"> 5 </a>
-          </li>
+  <?php if (have_posts()) : ?>
+
+
+    <section class="category-posts">
+      <div class="wrapper">
+        <h2 class="main-heading category-posts__h"> <?php echo $title; ?> </h2>
+        <ul class="posts-list">
+
+          <?php while (have_posts()) :
+            the_post(); ?>
+            <li class="last-post">
+              <a href="<?php the_permalink(); ?>" class="last-post__link" aria-label="Читать текст статьи: <?php the_title(); ?>">
+                <figure class="last-post__thumb">
+                  <?php the_post_thumbnail('full', ['class' => 'last-post__img']) ?>
+                </figure>
+                <div class="last-post__wrap">
+                  <h3 class="last-post__h"> <?php the_title(); ?> </h3>
+                  <p class="last-post__text"> <?php echo get_the_excerpt(); ?> </p>
+                  <span class="last-post__more link-more">Подробнее</span>
+                </div>
+              </a>
+            </li>
+
+          <?php endwhile; ?>
+
         </ul>
-      </nav>
-    </div>
-  </section>
+        <?php the_posts_pagination(); ?>
+      </div>
+    </section>
+
+  <?php
+  else :
+    get_template_part('tmp/no-posts.php');
+  endif;
+  ?>
+
+
 </main>
 
 <?php
