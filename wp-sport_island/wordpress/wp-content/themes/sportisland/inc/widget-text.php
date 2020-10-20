64f1691902b189dceecc8c0a0f615dab6623eb29
Dmitriy <shalberkind@mail.ru>
2020-10-20 10:54:04 +0300

    Исправлено поле ввода в админке

--- wp-sport_island/wordpress/wp-content/themes/sportisland/inc/widget-text.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/inc/widget-text.php
@@ -19,13 +19,13 @@ class SI_Widget_Text extends WP_Widget
       <label for="<?php echo $this->get_field_id('id-text') ?>">
         Введите текст:
       </label>
-      <input type="text" id="<?php echo $this->get_field_id('id-text') ?>" name="<?php echo $this->get_field_name('text'); ?>" value="<?php echo $instance['text']; ?>" class="widefat">
+      <textarea id="<?php echo $this->get_field_id('id-text') ?>" name="<?php echo $this->get_field_name('text'); ?>" class="widefat"> <?php echo $instance['text']; ?> </textarea>
     </p>
 <?php
   }
   public function widget($args, $instance)
   {
-    echo $instance['text'];
+    echo apply_filters('si_widget_text', $instance['text']);
   }
   public function update($new_instance, $old_instance)
   {
