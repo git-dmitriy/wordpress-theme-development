93dbac1e88aa87ef89b42a7ad7a8a447f1533da9
Dmitriy <shalberkind@mail.ru>
2020-10-21 16:34:18 +0300

    Оформление цикла wordpress

--- wp-sport_island/wordpress/wp-content/themes/sportisland/page-contacts.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/page-contacts.php
@@ -11,52 +11,54 @@ get_header();
   </div>
   <section class="contacts">
     <?php
-    while (have_posts()) :
-      the_post(); ?>
+    if (have_posts()) :
+      while (have_posts()) :
+        the_post(); ?>
 
-      <div class="wrapper">
-        <h1 class="contacts__h main-heading"><?php the_title(); ?></h1>
-        <div class="map">
-          <a href="#" class="map__fallback">
-            <img src="<?php echo _si_assets_path('img/map.jpg'); ?>" alt="Карта клуба SportIsland">
-            <span class="sr-only"> Карта </span>
-          </a>
-          <?php if (is_active_sidebar('si-map')) {
-            dynamic_sidebar('si-map');
-          }
-          ?>
+        <div class="wrapper">
+          <h1 class="contacts__h main-heading"><?php the_title(); ?></h1>
+          <div class="map">
+            <a href="#" class="map__fallback">
+              <img src="<?php echo _si_assets_path('img/map.jpg'); ?>" alt="Карта клуба SportIsland">
+              <span class="sr-only"> Карта </span>
+            </a>
+            <?php if (is_active_sidebar('si-map')) {
+              dynamic_sidebar('si-map');
+            }
+            ?>
+          </div>
+          <p class="contacts__info">
+            <?php if (is_active_sidebar('si-after-map')) {
+              dynamic_sidebar('si-after-map');
+            }
+            ?>
+          </p>
+
+          <?php the_content(); ?>
+          <form action="#" class="contacts__form contacts-form">
+            <label class="contacts-form__label">
+              <span class="sr-only"> Имя </span>
+              <input type="text" class="contacts-form__input" placeholder="Ваше имя">
+            </label>
+            <label class="contacts-form__label">
+              <span class="sr-only"> Телефон </span>
+              <input type="text" class="contacts-form__input" placeholder="Ваш телефон">
+            </label>
+            <label class="contacts-form__label">
+              <span class="sr-only"> Почта </span>
+              <input type="text" class="contacts-form__input" placeholder="Ваша почта">
+            </label>
+            <label class="contacts-form__label contacts-form__label_textarea">
+              <span class="sr-only"> Комментарий </span>
+              <textarea name="" id="" cols="30" rows="10" class="contacts-form__textarea" placeholder="Ваше сообщение"></textarea>
+            </label>
+            <button class="contacts-form__btn btn"> Отправить </button>
+          </form>
+
         </div>
-        <p class="contacts__info">
-          <?php if (is_active_sidebar('si-after-map')) {
-            dynamic_sidebar('si-after-map');
-          }
-          ?>
-        </p>
-
-        <?php the_content(); ?>
-        <form action="#" class="contacts__form contacts-form">
-          <label class="contacts-form__label">
-            <span class="sr-only"> Имя </span>
-            <input type="text" class="contacts-form__input" placeholder="Ваше имя">
-          </label>
-          <label class="contacts-form__label">
-            <span class="sr-only"> Телефон </span>
-            <input type="text" class="contacts-form__input" placeholder="Ваш телефон">
-          </label>
-          <label class="contacts-form__label">
-            <span class="sr-only"> Почта </span>
-            <input type="text" class="contacts-form__input" placeholder="Ваша почта">
-          </label>
-          <label class="contacts-form__label contacts-form__label_textarea">
-            <span class="sr-only"> Комментарий </span>
-            <textarea name="" id="" cols="30" rows="10" class="contacts-form__textarea" placeholder="Ваше сообщение"></textarea>
-          </label>
-          <button class="contacts-form__btn btn"> Отправить </button>
-        </form>
-
-      </div>
     <?php
-    endwhile;
+      endwhile;
+    endif;
     ?>
   </section>
 </main>
