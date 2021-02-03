2277f7bf832fdc3b7a5dd7a7570fef055f405039
Dmitriy <shalberkind@mail.ru>
2021-02-03 15:39:55 +0300

    Create templates for breadcumbs and no-posts sections

---
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/tmp/breadcrumbs.php
@@ -0,0 +1,8 @@
+<ul class="breadcrumbs">
+  <li class="breadcrumbs__item breadcrumbs__item_home">
+    <a href="index.html" class="breadcrumbs__link">Главная</a>
+  </li>
+  <li class="breadcrumbs__item">
+    <a href="blog.html" class="breadcrumbs__link">Блог</a>
+  </li>
+</ul>
---
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/tmp/no-posts.php
@@ -0,0 +1,5 @@
+<section class="last-posts">
+  <div class="wrapper">
+    <h2 class="main-heading last-posts__h"> Нет записей </h2>
+  </div>
+</section>
