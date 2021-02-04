f55fdc44a668323f542c4f7adb55685924dd2f2b
Dmitriy <shalberkind@mail.ru>
2021-02-04 23:46:11 +0300

    Output of ACF data for trainers

--- wp-sport_island/wordpress/wp-content/themes/sportisland/archive-trainers.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/archive-trainers.php
@@ -16,38 +16,37 @@ get_header();
   <section class="trainers">
     <div class="wrapper">
       <h1 class="main-heading trainers__h">Тренеры</h1>
-      <ul class="trainers-list">
-        <li class="trainers-list__item">
-          <article class="trainer">
-            <img src="img/trainers__trainer_pic1.png" alt="" class="trainer__thumb">
-            <div class="trainer__wrap">
-              <h2 class="trainer__name"> Комзова Ольга </h2>
-              <p class="trainer__text"> Сертифицированный специалист по Пилатесу (Matwork, Ring, Ball, miniball) и ZUMBA, ZUMBA by strong. </p>
-            </div>
-            <a href="#" class="trainer__subscribe btn">записаться</a>
-          </article>
-        </li>
-        <li class="trainers-list__item">
-          <article class="trainer">
-            <img src="img/trainers__trainer_pic2.png" alt="" class="trainer__thumb">
-            <div class="trainer__wrap">
-              <h2 class="trainer__name"> Юзвак Дмитрий </h2>
-              <p class="trainer__text"> Хореограф. Преподаватель Dance Mix, Twerk, Strip dance и др направлений. Официальный представитель UBPF. Чемпионка Европы по фитнес Бикини. Тренер по дефиле. Персональный тренер тренажерного зала. </p>
-            </div>
-            <a href="#" class="trainer__subscribe btn">записаться</a>
-          </article>
-        </li>
-        <li class="trainers-list__item">
-          <article class="trainer">
-            <img src="img/trainers__trainer_pic3.png" alt="" class="trainer__thumb">
-            <div class="trainer__wrap">
-              <h2 class="trainer__name"> Рудь Валерий </h2>
-              <p class="trainer__text"> Персональный тренер. МСМК по пауерлифтингу, неоднократный победитель соревнований. </p>
-            </div>
-            <a href="#" class="trainer__subscribe btn">записаться</a>
-          </article>
-        </li>
-      </ul>
+
+      <?php
+      if (have_posts()) :
+      ?>
+        <ul class="trainers-list">
+
+          <?php
+          while (have_posts()) :
+            the_post();
+          ?>
+
+            <li class="trainers-list__item">
+              <article class="trainer">
+                <img src="<?php echo get_field('trainers_pic')['url']; ?>" alt="<?php echo get_field('trainers_pic')['alt']; ?>" class="trainer__thumb">
+                <div class="trainer__wrap">
+                  <h2 class="trainer__name"> <?php the_title(); ?> </h2>
+                  <p class="trainer__text"><?php the_field('trainer_description'); ?></p>
+                </div>
+                <a href="#" class="trainer__subscribe btn">записаться</a>
+              </article>
+            </li>
+
+          <?php endwhile;  ?>
+
+        </ul>
+
+      <?php
+      else :
+        get_template_part('tmp/no-posts.php');
+      endif;
+      ?>
     </div>
   </section>
 </main>
