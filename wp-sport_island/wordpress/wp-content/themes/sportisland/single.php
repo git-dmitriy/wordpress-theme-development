3abb84ebd430919116253cbd50dc5c5344567add
Dmitriy <shalberkind@mail.ru>
2021-02-08 23:18:47 +0300

    Add frontend logic

--- wp-sport_island/wordpress/wp-content/themes/sportisland/single.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/single.php
@@ -21,7 +21,7 @@ get_header();
         <?php the_content(); ?>
         <footer class="main-article__footer">
           <time datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php echo get_the_date('d F Y'); ?></time>
-          <a href="#" class="main-article__like like">
+          <button class="main-article__like like" style="background-color: transparent; border: none; font-size: 16px; font-family:inherit; cursor:pointer;" data-href="<?php echo esc_url(admin_url('admin-ajax.php')); ?>" data-id="<?php echo $id; ?>">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 51.997 51.997" style="enable-background:new 0 0 51.997 51.997;" xml:space="preserve">
               <style>
                 path {
@@ -41,7 +41,81 @@ get_header();
               ?>
 
             </span>
-          </a>
+          </button>
+          <script>
+            window.addEventListener('load', function() {
+              const likeBtn = document.querySelector('.like');
+              const postID = likeBtn.getAttribute('data-id');
+              try {
+                if (!localStorage.getItem('liked')) {
+                  localStorage.setItem('liked', '');
+                }
+              } catch (e) {
+                console.log(e);
+              }
+
+              function getAboutLike(id) {
+                let hasLike = false;
+                try {
+                  hasLike = localStorage.getItem('liked').split(',').includes(id);
+
+                } catch (e) {
+                  console.log(e);
+                }
+                return hasLike;
+              }
+
+              let hasLike = getAboutLike(postID);
+
+              if (hasLike) {
+                likeBtn.classList.add('like_liked');
+              }
+
+              likeBtn.addEventListener('click', function(e) {
+                e.preventDefault();
+                likeBtn.disabled = true;
+
+                let hasLike = getAboutLike(postID);
+                const data = new FormData();
+                data.append('action', 'post-likes');
+
+                let todo = hasLike ? 'minus' : 'plus';
+                data.append('todo', todo);
+                data.append('id', postID);
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', likeBtn.getAttribute('data-href'));
+                xhr.send(data);
+                xhr.addEventListener('readystatechange', function() {
+                  if (xhr.readyState !== 4) return;
+                  if (xhr.status === 200) {
+                    likeBtn.querySelector('.like__count').innerText = xhr.responseText;
+                    let localData = localStorage.getItem('liked');
+                    let newData = '';
+                    if (hasLike) {
+                      newData = localData.split(',')
+                        .filter(function(el) {
+                          return el !== postID;
+                        })
+                        .join(',');
+                    } else {
+                      newData = localData.split(',')
+                        .filter(function(el) {
+                          return el !== '';
+                        })
+                        .concat(postID)
+                        .join(',');
+                    }
+
+                    localStorage.setItem('liked', newData);
+                    likeBtn.classList.toggle('like_liked');
+                  } else {
+                    console.log(xhr.statusText);
+                  }
+                  likeBtn.disabled = false;
+                })
+              })
+            });
+          </script>
         </footer>
       </article>
   <?php
