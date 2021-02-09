a286a2dbc5f22e8c53adc0c830c973639e040e14
Dmitriy <shalberkind@mail.ru>
2021-02-09 15:35:13 +0300

    Add data-id for modals

--- wp-sport_island/wordpress/wp-content/themes/sportisland/page-main.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/page-main.php
@@ -142,7 +142,7 @@ get_header();
                   </li>
                 <?php endforeach; ?>
               </ul>
-              <a data-post-id="99" href="#modal-form" class="card__buy btn btn_modal">купить</a>
+              <a data-post-id="<?php echo $id; ?>" href="#modal-form" class="card__buy btn btn_modal">купить</a>
             </li>
 
           <?php
