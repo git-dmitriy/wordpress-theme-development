ca9bc02cf04d8074ffb345d5e1e551d0a6e06ee0
Dmitriy <shalberkind@mail.ru>
2021-02-12 14:18:57 +0300

    Dsiplay cards

--- wp-sport_island/wordpress/wp-content/themes/sportisland/archive-prices.php
+++ wp-sport_island/wordpress/wp-content/themes/sportisland/archive-prices.php
@@ -66,50 +66,69 @@ get_header();
         </tbody>
       </table>
     </section>
-    <section class="cards">
-      <h2 class="page-heading cards__h"> клубные карты </h2>
-      <ul class="cards__list row">
-        <li class="card">
-          <h3 class="card__name"> полный день </h3>
-          <p class="card__time"> 7:00 &ndash; 22:00 </p>
-          <p class="card__price price"> 3200 <span class="price__unit">р.-/мес.</span>
-          </p>
-          <ul class="card__features">
-            <li class="card__feature">Безлимит посещений клуба</li>
-            <li class="card__feature">Вводный инструктаж</li>
-            <li class="card__feature">Групповые занятия</li>
-            <li class="card__feature">Сауна</li>
-          </ul>
-          <a href="#" class="card__buy btn">купить</a>
-        </li>
-        <li class="card card_profitable">
-          <h3 class="card__name"> полный день </h3>
-          <p class="card__time"> 7:00 &ndash; 22:00 </p>
-          <p class="card__price price"> 3200 <span class="price__unit">р.-/мес.</span>
-          </p>
-          <ul class="card__features">
-            <li class="card__feature">Безлимит посещений клуба</li>
-            <li class="card__feature">Вводный инструктаж</li>
-            <li class="card__feature">Групповые занятия</li>
-            <li class="card__feature">Сауна</li>
-          </ul>
-          <a href="#" class="card__buy btn">купить</a>
-        </li>
-        <li class="card">
-          <h3 class="card__name"> полный день </h3>
-          <p class="card__time"> 7:00 &ndash; 22:00 </p>
-          <p class="card__price price"> 3200 <span class=" price__unit">р.-/мес.</span>
-          </p>
-          <ul class="card__features">
-            <li class="card__feature">Безлимит посещений клуба</li>
-            <li class="card__feature">Вводный инструктаж</li>
-            <li class="card__feature">Групповые занятия</li>
-            <li class="card__feature">Сауна</li>
-          </ul>
-          <a href="#" class="card__buy btn">купить</a>
-        </li>
-      </ul>
-    </section>
+
+    <?php
+    $query = new WP_Query([
+      'numberposts' => -1,
+      'post_type' => 'cards',
+      'meta_key' => 'club_order',
+      'orderby' => 'meta_value_num',
+      'order' => 'ASC'
+    ]);
+    if ($query->have_posts()) :
+    ?>
+
+      <section class="cards">
+        <h2 class="main-heading cards__h"> клубные карты </h2>
+        <ul class="cards__list row">
+          <?php
+          while ($query->have_posts()) :
+            $query->the_post();
+            $profit_class = '';
+            if (get_field('club_profit')) {
+              $profit_class = " card_profitable";
+            }
+            $benefits = get_field('club_benefits');
+            $benefits = explode("\n", $benefits);
+
+            $bg = get_field('club_bg');
+            $bg_default = _si_assets_path('img/index__cards_card3.jpg');
+            $bg = $bg ?
+              "style=\"background-image: url(${bg});\"" :
+              "style=\"background-image: url(${bg_default});\"";
+          ?>
+            <li class="card <?php echo $profit_class; ?>" <?php echo $bg ?>>
+              <h3 class="card__name">
+                <?php the_title(); ?>
+              </h3>
+              <p class="card__time">
+                <?php the_field('club_time_start'); ?>
+                &ndash;
+                <?php the_field('club_time_end'); ?>
+              </p>
+              <p class="card__price price">
+                <?php the_field('club_price'); ?>
+                <span class="price__unit" aria-label="рублей в месяц">р.-/мес.</span>
+              </p>
+              <ul class="card__features">
+                <?php foreach ($benefits as $bn) : ?>
+                  <li class="card__feature">
+                    <?php echo $bn; ?>
+                  </li>
+                <?php endforeach; ?>
+              </ul>
+              <a data-post-id="<?php echo $id; ?>" href="#modal-form" class="card__buy btn btn_modal">купить</a>
+            </li>
+          <?php
+          endwhile;
+          wp_reset_postdata();
+          ?>
+        </ul>
+      </section>
+    <?php
+    endif;
+    ?>
+
   </div>
 </main>
 
